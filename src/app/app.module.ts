cdc61c59ce3c0856af1199c689eb48c989bb783f
Chris Pawlukiewicz <hi@paynoattn.com>
2018-03-16 20:19:50 -0600

    theme and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material';
 
 
 import { AppComponent } from './app.component';
@@ -10,8 +12,10 @@ import { PlayerModule } from './player/player.module';
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    PlayerModule
+    PlayerModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
