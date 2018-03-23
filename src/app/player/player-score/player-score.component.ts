f6eb2e12900efe390f8aaa9d49ce6162deaf2ceb
Chris Pawlukiewicz <hi@paynoattn.com>
2018-03-23 12:12:19 -0600

    wip displaying scores

--- src/app/player/player-score/player-score.component.ts
+++ src/app/player/player-score/player-score.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 import { Player } from '../player.model';
 import { PlayerScore } from './player-score.model';
+import { PlayerScoreService } from './player-score.service';
 
 @Component({
   selector: 'app-player-score',
   templateUrl: './player-score.component.html',
   styleUrls: ['./player-score.component.scss']
 })
-export class PlayerScoreComponent {
+export class PlayerScoreComponent implements OnChanges {
+  @Input() scores: PlayerScore[];
+  public calucalatedScore: number[] = [];
+
+  constructor(private scoreSvc: PlayerScoreService) { }
+
+  ngOnChanges() {
+    if (this.scores) { console.log('I have the scores', this.scores); }
+  }
 }
